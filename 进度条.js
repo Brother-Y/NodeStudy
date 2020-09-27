6edacb3fcfb6b8494e19402a0c6c412a2fb94688
Brother-Y <450028105@qq.com>
2020-09-27 16:37:45 +0800

    node重新学习

---
+++ 进度条.js
@@ -0,0 +1,21 @@
+const chalk = require('chalk');
+console.log(chalk.red('hello world'));
+const ProgressBar = require('progress')
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+  
+readline.question(`你叫什么名字?`, name => {
+    console.log(`你好 ${name}!`)
+    readline.close()
+})
+const bar = new ProgressBar(':bar', { total: 10 })
+const timer = setInterval(() => {
+  bar.tick()
+  if (bar.complete) {
+    clearInterval(timer)
+  }
+}, 100)
+
