04d34ffcd5880e8d2d11dc6d099843794a950e2a
Brother-Y <450028105@qq.com>
2020-09-28 09:30:14 +0800

    http搭建本地服务器

--- 进度条.js
+++ 进度条.js
@@ -5,7 +5,7 @@ const ProgressBar = require('progress')
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
-  })
+})
   
 readline.question(`你叫什么名字?`, name => {
     console.log(`你好 ${name}!`)
